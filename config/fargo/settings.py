8dc8e451010619b97f81f3fc6449282053e6176c
Cédric De Wilde <cedric.dewilde@imio.be>
2016-06-02 14:05:11 +0200

    enable memcached

--- config/fargo/settings.py
+++ config/fargo/settings.py
@@ -35,6 +35,14 @@ DATABASES['default']['PASSWORD'] = 'password'
 DATABASES['default']['HOST'] = 'database'
 DATABASES['default']['PORT'] = '5432'
 
+CACHES = {
+    'default': {
+        'BACKEND': 'hobo.multitenant.cache.TenantCache',
+        'REAL_BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '172.17.0.1:11211',
+    }
+}
+
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Europe/Brussels'
 
--- config/hobo-agent/settings.py
+++ config/hobo-agent/settings.py
@@ -16,6 +16,14 @@ AGENT_HOST_PATTERNS = {
     'fargo': ['local-documents.example.net'],
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'hobo.multitenant.cache.TenantCache',
+        'REAL_BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '172.17.0.1:11211',
+    }
+}
+
 WCS_MANAGE_COMMAND = 'sudo -u wcs-au-quotidien /usr/sbin/wcsctl -f /etc/wcs/wcs-au-quotidien.cfg'
 AUTHENTIC_MANAGE_COMMAND = 'sudo -u authentic-multitenant /usr/bin/authentic2-multitenant-manage'
 COMBO_MANAGE_COMMAND = 'sudo -u combo /usr/bin/combo-manage'
