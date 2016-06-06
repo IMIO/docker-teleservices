f53c168d95a00c4f8e5b8ee25e94fc4a21fc1df8
Christophe Boulanger <christophe.boulanger@imio.be>
2016-06-06 13:49:01 +0200

    Add new validation on phone number profile

--- config/authentic2/config.py
+++ config/authentic2/config.py
@@ -113,6 +113,18 @@ class NumHouseField(forms.CharField):
         except ValueError:
             raise forms.ValidationError("Format invalide")
 
+class NumPhoneField(forms.CharField):
+    def validate(self, value):
+        super(NumPhoneField, self).validate(value)
+        if not value:
+            return
+        try:
+            if not re.match("^(0|\\+|00)(\d{8,}", value):
+                raise ValueError()
+        except ValueError:
+            raise forms.ValidationError("Format invalide")
+
+
 
 A2_ATTRIBUTE_KINDS = [
         {
@@ -124,6 +136,11 @@ A2_ATTRIBUTE_KINDS = [
             'label': u'Numéro de maison',
             'name': 'num_house',
             'field_class': NumHouseField,
+        },
+        {
+            'label': u'Numéro de téléphone',
+            'name': 'phone',
+            'field_class': NumPhoneField,
         }
 ]
 
