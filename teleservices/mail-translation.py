48599a25a92d760515a5b57199b19e4449297159
Frédéric Péters <fpeters@entrouvert.com>
2016-06-30 09:24:08 +0200

    translations: add custom translation for wrong credentials essage (#14144)

--- teleservices/mail-translation.py
+++ teleservices/mail-translation.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 
@@ -8,6 +9,18 @@ contents = contents.replace('adresse de courriel', 'adresse e-mail'
                   ).replace('"Courriel"', '"Adresse e-mail"')
 fd = open('/var/lib/authentic2/locale/fr/LC_MESSAGES/django.po', 'w')
 fd.write(contents)
+
+print >> fd, """
+#, python-format
+msgid ""
+"Please enter a correct %(username)s and password. Note that both fields may "
+"be case-sensitive."
+msgstr ""
+"L’adresse e-mail et le mot de passe que vous avez saisis ne correspondent "
+"pas. Si vous n'avez pas encore créé de compte, cliquez sur le lien "
+"« Enregistrez-vous »."
+"""
+
 fd.close()
 
 os.system('msgfmt -o /var/lib/authentic2/locale/fr/LC_MESSAGES/django.mo /var/lib/authentic2/locale/fr/LC_MESSAGES/django.po')
