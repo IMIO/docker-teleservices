90cd8b8718cb7bc310490b86adf0c8a2c358e3b0
Frédéric Péters <fpeters@entrouvert.com>
2016-07-14 14:59:05 +0200

    use a real .po file to override authentic translations

--- teleservices/mail-translation.py
+++ teleservices/mail-translation.py
@@ -2,26 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 
 contents = open('/usr/share/pyshared/authentic2/locale/fr/LC_MESSAGES/django.po').read()
 contents = contents.replace('adresse de courriel', 'adresse e-mail'
                   ).replace('Nouveau courriel', 'Nouvelle adresse e-mail'
                   ).replace('"Courriel"', '"Adresse e-mail"')
-fd = open('/var/lib/authentic2/locale/fr/LC_MESSAGES/django.po', 'w')
+fd = open('/var/lib/authentic2/locale/fr/LC_MESSAGES/authentic.po', 'w')
 fd.write(contents)
-
-print >> fd, """
-#, python-format
-msgid ""
-"Please enter a correct %(username)s and password. Note that both fields may "
-"be case-sensitive."
-msgstr ""
-"L’adresse e-mail et le mot de passe que vous avez saisis ne correspondent "
-"pas. Si vous n'avez pas encore créé de compte, cliquez sur le lien "
-"« Enregistrez-vous »."
-"""
-
 fd.close()
 
-os.system('msgfmt -o /var/lib/authentic2/locale/fr/LC_MESSAGES/django.mo /var/lib/authentic2/locale/fr/LC_MESSAGES/django.po')
+subprocess.call([
+    'msgcat',
+    '--use-first',
+    '/var/lib/authentic2/locale/fr/LC_MESSAGES/overrides.po',
+    '/var/lib/authentic2/locale/fr/LC_MESSAGES/authentic.po',
+    '--output',
+    '/var/lib/authentic2/locale/fr/LC_MESSAGES/django.po'])
 
+subprocess.call([
+    'msgfmt',
+    '-o',
+    '/var/lib/authentic2/locale/fr/LC_MESSAGES/django.mo',
+    '/var/lib/authentic2/locale/fr/LC_MESSAGES/django.po'])
