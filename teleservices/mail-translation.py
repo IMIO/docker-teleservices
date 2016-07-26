99a70661961af40667e1a75fb2078c1644d3d3ca
Christophe Boulanger <christophe.boulanger@imio.be>
2016-07-26 09:04:43 +0200

    Add replacement of lowcase 'courriel'

--- teleservices/mail-translation.py
+++ teleservices/mail-translation.py
@@ -11,7 +11,8 @@ else:
     contents = open('/usr/lib/python2.7/dist-packages/authentic2/locale/fr/LC_MESSAGES/django.po').read()
 contents = contents.replace('adresse de courriel', 'adresse e-mail'
                   ).replace('Nouveau courriel', 'Nouvelle adresse e-mail'
-                  ).replace('"Courriel"', '"Adresse e-mail"')
+                  ).replace('courriel', 'adresse e-mail'
+                  ).replace('Courriel', 'Adresse e-mail')
 fd = open('/var/lib/authentic2/locale/fr/LC_MESSAGES/authentic.po', 'w')
 fd.write(contents)
 fd.close()
