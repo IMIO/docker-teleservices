e36ae45cd441c1ce16495da0b094b512ca3ac5fd
Frédéric Péters <fpeters@entrouvert.com>
2016-09-09 10:00:48 +0200

    remove erroneous copy/pasted visible tabs

--- src/scripts/build-sass.sh
+++ src/scripts/build-sass.sh
@@ -7,10 +7,10 @@ cd /opt/publik/imio-publik-themes
 while /bin/true
 do
 	sass \
->--->---static/test/style.scss:static/test/style.css \
+		static/test/style.scss:static/test/style.css \
 		static/liege/style.scss:static/liege/style.css \
 		static/namur/style.scss:static/namur/style.css \
 		static/lalouviere/style.scss:static/lalouviere/style.css \
->--->---static/huy/style.scss:static/huy/style.css
+		static/huy/style.scss:static/huy/style.css
 	sleep 5
 done
