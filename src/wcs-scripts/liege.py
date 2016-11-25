69df05687107dd8826d164a8ee3982efc960932c
Christophe Boulanger <christophe.boulanger@imio.be>
2016-11-25 09:23:44 +0100

    reindent liege.py and town.py

--- src/wcs-scripts/liege.py
+++ src/wcs-scripts/liege.py
@@ -4,7 +4,7 @@ import re
 sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
 
 if 'town' in sys.modules:
-       del sys.modules['town']
+    del sys.modules['town']
 
 import town
 
@@ -13,124 +13,124 @@ liege_cities = set(['Bressoux', 'Chénée', 'Glain', 'Grivegnée', 'Jupille', 'L
 
 class Liege(town.Town):
 
-  def __init__(self):
-    super(Liege, self).__init__(variables=globals())
+    def __init__(self):
+        super(Liege, self).__init__(variables=globals())
 
-  def is_not_liege_filtered_list(self, choices, is_not_liege_filtered_list = [], forms_exceptions= []):
-    if not self.user_zipcode:
-      return choices
-    if self.user_zipcode in ('4000', '4020', '4030', '4031', '4032'):
-      return choices
-    else:
-      if self.form_slug in forms_exceptions:
-        return choices
-      else:
-        return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
+    def is_not_liege_filtered_list(self, choices, is_not_liege_filtered_list = [], forms_exceptions= []):
+        if not self.user_zipcode:
+            return choices
+        if self.user_zipcode in ('4000', '4020', '4030', '4031', '4032'):
+            return choices
+        else:
+            if self.form_slug in forms_exceptions:
+                return choices
+            else:
+                return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
 
-  def is_liege_resident(self, choices, is_liege_filtered_list = [], is_not_liege_filtered_list = []):
-    if not self.user_zipcode:
-      return choices
-    if self.user_zipcode in ('4000', '4020', '4030', '4031', '4032'):
-      if len(is_liege_filtered_list) > 0:
-        return [x for i, x in enumerate(choices) if i in is_liege_filtered_list]
-      else:
-        return choices
-    else:
-      if len(is_not_liege_filtered_list) > 0:
-        return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
-      else:
-        return choices
+    def is_liege_resident(self, choices, is_liege_filtered_list = [], is_not_liege_filtered_list = []):
+        if not self.user_zipcode:
+            return choices
+        if self.user_zipcode in ('4000', '4020', '4030', '4031', '4032'):
+            if len(is_liege_filtered_list) > 0:
+                return [x for i, x in enumerate(choices) if i in is_liege_filtered_list]
+            else:
+                return choices
+        else:
+            if len(is_not_liege_filtered_list) > 0:
+                return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
+            else:
+                return choices
 
-  def verif_liege_wedding(self, wedding_cities):
-    if wedding_cities in liege_cities or wedding_cities is None:
-      return True
-    else:
-      return False
-
-  def had_liege_wedding(self, choices, is_not_liege_filtered_list = []):
-    if not self.user_wedding_cities:
-      return choices
-    if liege_cities.intersection(self.user_wedding_cities.split('|')):
-      return choices
-    else:
-      return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
-
-  def has_liege_birthplace(self, choices, is_not_liege_filtered_list = []):
-    if not self.user_birthplace:
-      return choices
-    if self.user_birthplace in liege_cities:
-      return choices
-    else:
-      return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
-
-
-  def test_liege_and_return_text(self, is_liege_text, is_not_liege_text):
-    if self.user_zipcode  in ('4000', '4020', '4030', '4031', '4032'):
-      return is_liege_text
-    else:
-      return is_not_liege_text
-
-  def validate_dynamic_tab_cells(self, table_var, id_colonne, regex_pattern, id_row = "-1"):
-    retour = True
-    id_col = int(id_colonne)
-    try:
-      if id_row == "-1":
-        for item in table_var:
-          value = item[id_col]
-          if value == '' or value is None:
-            value = "0"
-          if re.match(regex_pattern, value) is None:
+    def verif_liege_wedding(self, wedding_cities):
+        if wedding_cities in liege_cities or wedding_cities is None:
+            return True
+        else:
             return False
-      else:
-        id_r = int(id_row)
-        if re.match(regex_pattern,table_var[id_r][id_col]) is None:
-          retour = False
-      return retour
-    except:
-      return False
 
-  # This is a very specific function to validate a dynamic table in a specific form for LIEGE
-  # This form is "Commande courrier/fax/mail"
-  def validate_specific_computing_tab(self, table_var, lst_cols1, lst_cols2):
-    retour = True
-    try:
-      for item in table_var:
-        index_elems_to_convert = lst_cols1 + lst_cols2
-        i = 0
-        new_list = []
-        while i < len(item):
-          value = item[i]
-          if i in index_elems_to_convert:
-            if value == '':
-              value = value.replace("","0")
-            value = int(value)
-          new_list.append(value)
-          i = i + 1
-        sum_cols1_values = sum([new_list[index] for index in lst_cols1])
-        sum_cols2_values = sum([new_list[index] for index in lst_cols2])
-        if sum_cols1_values != sum_cols2_values:
-          retour = False
-      return retour
-    except:
-      return False
+    def had_liege_wedding(self, choices, is_not_liege_filtered_list = []):
+        if not self.user_wedding_cities:
+            return choices
+        if liege_cities.intersection(self.user_wedding_cities.split('|')):
+            return choices
+        else:
+            return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
 
-  def simplified_belgium_iban_validator(self, iban_number):
-    result = False
-    key = iban_number[2:4]
-    if len(iban_number) == 16:
-      if iban_number[0:2] == "BE":
-        B = "11"
-        E = "14"
-        if re.match("^[0-9]*$", iban_number[2:16]):
-          valid_key = 98 - (int(iban_number[4:16] + B + E + "00") % 97)
-          if int(key) == valid_key:
-            tmp_iban_number = iban_number[4:16] + B + E + iban_number[2:4]
-            if int(tmp_iban_number) % 97 == 1:
-              result = True
-    return result
+    def has_liege_birthplace(self, choices, is_not_liege_filtered_list = []):
+        if not self.user_birthplace:
+            return choices
+        if self.user_birthplace in liege_cities:
+            return choices
+        else:
+            return [x for i, x in enumerate(choices) if i in is_not_liege_filtered_list]
 
-  def test(self, a):
-    return type(Town)
+
+    def test_liege_and_return_text(self, is_liege_text, is_not_liege_text):
+        if self.user_zipcode  in ('4000', '4020', '4030', '4031', '4032'):
+            return is_liege_text
+        else:
+            return is_not_liege_text
+
+    def validate_dynamic_tab_cells(self, table_var, id_colonne, regex_pattern, id_row = "-1"):
+        retour = True
+        id_col = int(id_colonne)
+        try:
+            if id_row == "-1":
+                for item in table_var:
+                    value = item[id_col]
+                    if value == '' or value is None:
+                        value = "0"
+                    if re.match(regex_pattern, value) is None:
+                        return False
+            else:
+                id_r = int(id_row)
+                if re.match(regex_pattern,table_var[id_r][id_col]) is None:
+                    retour = False
+            return retour
+        except:
+            return False
+
+    # This is a very specific function to validate a dynamic table in a specific form for LIEGE
+    # This form is "Commande courrier/fax/mail"
+    def validate_specific_computing_tab(self, table_var, lst_cols1, lst_cols2):
+        retour = True
+        try:
+            for item in table_var:
+                index_elems_to_convert = lst_cols1 + lst_cols2
+                i = 0
+                new_list = []
+                while i < len(item):
+                    value = item[i]
+                    if i in index_elems_to_convert:
+                        if value == '':
+                            value = value.replace("","0")
+                        value = int(value)
+                    new_list.append(value)
+                    i = i + 1
+                sum_cols1_values = sum([new_list[index] for index in lst_cols1])
+                sum_cols2_values = sum([new_list[index] for index in lst_cols2])
+                if sum_cols1_values != sum_cols2_values:
+                    retour = False
+            return retour
+        except:
+            return False
+
+    def simplified_belgium_iban_validator(self, iban_number):
+        result = False
+        key = iban_number[2:4]
+        if len(iban_number) == 16:
+            if iban_number[0:2] == "BE":
+                B = "11"
+                E = "14"
+                if re.match("^[0-9]*$", iban_number[2:16]):
+                    valid_key = 98 - (int(iban_number[4:16] + B + E + "00") % 97)
+                    if int(key) == valid_key:
+                        tmp_iban_number = iban_number[4:16] + B + E + iban_number[2:4]
+                        if int(tmp_iban_number) % 97 == 1:
+                            result = True
+        return result
+
+    def test(self, a):
+        return type(Town)
 
 current_commune = Liege()
 # args[0] is the function name calling in WCS
@@ -138,11 +138,11 @@ function = args[0]
 
 functionList = {function: getattr(current_commune,function)}
 if args[1] is not None:
-  parameters = args[1]
-  if isinstance(parameters, dict):
-    result = functionList[function](**parameters)
-  else:
-    params = args[1:]
-    result = functionList[function](*params)
+    parameters = args[1]
+    if isinstance(parameters, dict):
+        result = functionList[function](**parameters)
+    else:
+        params = args[1:]
+        result = functionList[function](*params)
 else:
-  result = functionList[function]()
+    result = functionList[function]()
--- src/wcs-scripts/town.py
+++ src/wcs-scripts/town.py
@@ -3,52 +3,89 @@ import datetime
 import re
 import pdb
 
+
 class Town(object):
 
-  def __init__(self, variables):
-    self.class_name = 'Town'
-    self.form_name = variables.get('form_name')
-    self.user_zipcode = variables.get('session_user_var_zipcode')
-    self.user_birthplace = variables.get('session_user_var_birthplace')
-    self.user_wedding_cities = variables.get('session_user_var_wedding_cities') or ''
-    self.user_title = variables.get('session_user_var_title')
-    try:
-      self.form_slug = variables.get('form_slug')
-    except NameError:
-      self.form_slug = None
+    def __init__(self, variables):
+        self.class_name = 'Town'
+        self.form_name = variables.get('form_name')
+        self.user_zipcode = variables.get('session_user_var_zipcode')
+        self.user_birthplace = variables.get('session_user_var_birthplace')
+        self.user_wedding_cities = variables.get('session_user_var_wedding_cities') or ''
+        self.user_title = variables.get('session_user_var_title')
+        try:
+            self.form_slug = variables.get('form_slug')
+        except NameError:
+            self.form_slug = None
 
-  def test(self):
-    return self.__dict__
+    def get_form_slug(self):
+        return self.form_slug
 
-  def txt_ifn_zero(self, valeur, texte, coeff = 1):
-    if str(valeur) == "0":
-      return ""
-    else:
-      return str(valeur) + " " + texte + " = " + str(int(valeur) * coeff)+ ""
+    def get_form_name(self):
+        return self.form_name
 
-  def criteria_filtered_list(self, choices, value_to_test, criteria_to_test, choices_if_true, choices_if_false):
-    if str(choices) not in choices_if_false:
-        # patch for attestatin de milice Liege ... TODO 
-        choices_if_false = [0]
-    if value_to_test == criteria_to_test:
-      return [x for i, x in enumerate(choices) if i in choices_if_true]
-    else:
-      return [x for i, x in enumerate(choices) if i in choices_if_false]
+    def txt_ifn_zero(self, valeur, texte, coeff = 1):
+        if str(valeur) == "0":
+            return ""
+        else:
+            return str(valeur) + " " + texte + " = " + str(int(valeur) * coeff)+ ""
 
-  def get_form_slug(self):
-     return self.form_slug
+    def criteria_filtered_list(self, choices, value_to_test, criteria_to_test, choices_if_true, choices_if_false):
+        if str(choices) not in choices_if_false:
+            # patch for attestatin de milice Liege ... TODO 
+            choices_if_false = [0]
+        if value_to_test == criteria_to_test:
+            return [x for i, x in enumerate(choices) if i in choices_if_true]
+        else:
+            return [x for i, x in enumerate(choices) if i in choices_if_false]
 
-  def get_form_name(self):
-     return self.form_name
+    def diff_dates(self, date1, date2):
+        try:
+            d1 = datetime.datetime.strptime(date1, '%d/%m/%Y')
+            d2 = datetime.datetime.strptime(date2, '%d/%m/%Y')
+            diff = (d2 - d1).days
+            return str(diff)
+        except:
+            return "diff_dates_error"
 
-  def diff_dates(self, date1, date2):
-    try:
-      d1 = datetime.datetime.strptime(date1, '%d/%m/%Y')
-      d2 = datetime.datetime.strptime(date2, '%d/%m/%Y')
-      diff = (d2 - d1).days
-      return str(diff)
-    except:
-      return "diff_dates_error"
+    # Try to sum each row from a given column in a table (each value from the given column must be integer)
+    # table_var : this is the variable name of the table in forms.
+    # id_colonne : the id of the column in the table!
+    # price : default value = 1.
+    # return =  [sum of all row in given column] * price.
+    def compute_dynamic_tab(self, table_var, id_colonne, price = 1):
+        result = 0
+        if table_var is None:
+            return str(result)
+        else:
+            try:
+                id_col = int(id_colonne)
+                for item in table_var:
+                    value = item[id_col]
+                    if value == "":
+                        value = "0"
+                    result = result + int(value)
+                result = int(result) * int(price)
+                return str(result)
+            except:
+                return "compute_dynamic_tab : error" + str(table_var)
+
+    def compute_tab_col(self, tab_var, num_col, memory_tab):
+        error1 = "Erreur compute_tab_col : tableau en mem. different que tableau du formulaire."
+        result = 0
+        if len(tab_var) != len(memory_tab):
+            result = error1
+        else:
+            cpt = 0
+            for item in tab_var:
+                try:
+                    if item[int(num_col)] is None:
+                        item = '0'
+                    result = result + (int(item[int(num_col)]) * int(memory_tab[cpt][2]))
+                    cpt = cpt + 1
+                except:
+                    result = "Erreur compute_tab_col"
+        return str(result)
 
 # à valider.
 #  def is_valid_iban(self, iban):
@@ -75,43 +112,3 @@ class Town(object):
 #      subpart = str(previousModulo) + "" + numericIbanString[i:i + 5]
 #      previousModulo = int(subpart) % ibanValidationModulo
 #    return previousModulo == 1
-
-  # Try to sum each row from a given column in a table (each value from the given column must be integer)
-  # table_var : this is the variable name of the table in forms.
-  # id_colonne : the id of the column in the table!
-  # price : default value = 1.
-  # return =  [sum of all row in given column] * price.
-  def compute_dynamic_tab(self, table_var, id_colonne, price = 1):
-    result = 0
-    if table_var is None:
-      return str(result)
-    else:
-      try:
-        id_col = int(id_colonne)
-        for item in table_var:
-          value = item[id_col]
-          if value == "":
-            value = "0"
-          result = result + int(value)
-        result = int(result) * int(price)
-        return str(result)
-      except:
-        return "compute_dynamic_tab : error" + str(table_var)
-
-
-  def compute_tab_col(self, tab_var, num_col, memory_tab):
-    error1 = "Erreur compute_tab_col : tableau en mem. different que tableau du formulaire."
-    result = 0
-    if len(tab_var) != len(memory_tab):
-      result = error1
-    else:
-      cpt = 0
-      for item in tab_var:
-        try:
-          if item[int(num_col)] is None:
-            item = '0'
-          result = result + (int(item[int(num_col)]) * int(memory_tab[cpt][2]))
-          cpt = cpt + 1
-        except:
-          result = "Erreur compute_tab_col"
-    return str(result)
