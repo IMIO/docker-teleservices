7226c160c99d5059ae31ecb01f3157b43a060cdb
Frédéric Péters <fpeters@entrouvert.com>
2017-01-17 09:59:46 +0100

    separate street from boite number

--- src/wcs-scripts/dest_ligne2.py
+++ src/wcs-scripts/dest_ligne2.py
@@ -2,4 +2,7 @@ import sys
 sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
 import dest
 reload(dest)
-result = '%(rue)s %(numero)s %(boite)s' % dest.get(globals())
+if boite:
+    result = '%(rue)s %(numero)s / %(boite)s' % dest.get(globals())
+else:
+    result = '%(rue)s %(numero)s' % dest.get(globals())
