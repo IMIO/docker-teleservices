9ae530fb06d2153a33f36e083f8d9d257db7bc94
Frédéric Péters <fpeters@entrouvert.com>
2017-02-02 14:22:10 +0100

    liege: fix check on boite

--- src/wcs-scripts/dest_ligne2.py
+++ src/wcs-scripts/dest_ligne2.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
 import dest
 reload(dest)
-if boite:
+if dest.get(globals()).get('boite'):
     result = '%(rue)s %(numero)s / %(boite)s' % dest.get(globals())
 else:
     result = '%(rue)s %(numero)s' % dest.get(globals())
