cb3ad012f4410bc93993ef4480df9ae4a4404a1e
Christophe Boulanger <christophe.boulanger@imio.be>
2017-03-03 09:42:03 +0100

    Oupeye : add oupeye.py for wcs_script

---
+++ src/wcs-scripts/oupeye.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
+import re
+import pdb
+
+if 'town' in sys.modules:
+    del sys.modules['town']
+
+import town
+
+
+class Oupeye(town.Town):
+
+    def __init__(self):
+        super(Namur, self).__init__(variables=globals())
+
+current_commune = Oupeye()
+function = args[0]
+
+functionList = {function: getattr(current_commune,function)}
+if args[1] is not None:
+    parameters = args[1]
+    if isinstance(parameters, dict):
+        result = functionList[function](**parameters)
+    else:
+        params = args[1:]
+        result = functionList[function](*params)
+else:
+    result = functionList[function]()
