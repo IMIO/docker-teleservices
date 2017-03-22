371ba039fdb725a54161d7c937aee04436715d0b
Frédéric Péters <fpeters@entrouvert.com>
2017-03-22 11:24:48 +0100

    replace database creation by a SQL script

--- data/docker-entrypoint-initdb.d/createDB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-su postgres -c "createdb bijoe"
-su postgres -c "createdb combo"
-su postgres -c "createdb hobo"
-su postgres -c "createdb passerelle"
-su postgres -c "createdb authentic2"
-su postgres -c "createdb fargo"
-su postgres -c "createdb wcs"
-
