889f291b30fd3222958e12cbff7c296578c9abbd
Christophe Boulanger <christophe.boulanger@imio.be>
2017-04-04 09:43:28 +0200

    Remove pdb out of import-workflows script

--- src/scripts/migration-ts1/import-workflows.py
+++ src/scripts/migration-ts1/import-workflows.py
@@ -7,7 +7,6 @@ from wcs.workflows import Workflow
 # default path : "/opt/publik/scripts/migration-ts1/workflows/"
 folder_path = sys.argv[1]
 for fichier in os.listdir(folder_path):
-    import pdb;pdb.set_trace()
     if fichier[-4:] == ".wcs":
         print "{}{}".format(folder_path, fichier)
         fd = open("{}{}".format(folder_path, fichier))
