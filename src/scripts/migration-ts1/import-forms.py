dfe925def57cee29b27e72fb6678c92a7e1ff683
Christophe Boulanger <christophe.boulanger@imio.be>
2017-06-30 14:34:59 +0200

    When launch import forms' script 2 times, avoid to rewrite same forms two times

--- src/scripts/migration-ts1/import-forms.py
+++ src/scripts/migration-ts1/import-forms.py
@@ -2,32 +2,45 @@ import os
 import sys
 from wcs.formdef import FormDef
 lst_formdef_ids = []
+lst_formdef_names = []
 conv = lambda x: int(x)
 for formdef in FormDef.select():
+    lst_formdef_names.append(formdef.name)
     lst_formdef_ids.append(formdef.id)
 # default folder path : # "/opt/publik/scripts/migration-ts1/forms/"
 folder_path = sys.argv[1]
 for fichier in os.listdir(folder_path):
     if fichier[-4:] == ".wcs":
         fd = open("{}{}".format(folder_path, fichier))
+        print fichier
         formdef = FormDef.import_from_xml(fd, charset='utf-8', include_id=False)
-        formdef.disabled = False
-        if formdef.category.id == "99":
-            with open("/tmp/tmp_uuid_agent_traitant_pop.txt", 'r') as file_at:
-                uuid_at = file_at.read()
-                formdef.workflow_roles = {'_receiver':uuid_at}
-        else:
-            with open("/tmp/tmp_uuid_agent_traitant_trav.txt", 'r') as file_at:
-                uuid_at = file_at.read()
-                formdef.workflow_roles = {'_receiver':uuid_at}
-        try:
-            if len(lst_formdef_ids) < 1:
-                new_id = 1
+        print formdef.name
+        if formdef.name not in lst_formdef_names:
+            formdef.disabled = False
+            if formdef.category.id == "99":
+                try:
+                    with open("/tmp/tmp_uuid_agent_traitant_pop.txt", 'r') as file_at:
+                        uuid_at = file_at.read()
+                        formdef.workflow_roles = {'_receiver':uuid_at}
+                except:
+                        uuid_at = 12
+                        formdef.workflow_roles = {'_receiver':uuid_at}
             else:
-                new_id = int(sorted(lst_formdef_ids, key=conv)[-1]) + 1
-            lst_formdef_ids.append(new_id)
-            formdef.id = new_id
-            formdef.store()
-        except:
-            print "import error : " + formdef.id
+                try:
+                    with open("/tmp/tmp_uuid_agent_traitant_trav.txt", 'r') as file_at:
+                        uuid_at = file_at.read()
+                        formdef.workflow_roles = {'_receiver':uuid_at}
+                except:
+                        uuid_at = 13
+                        formdef.workflow_roles = {'_receiver':uuid_at}
+            try:
+                if len(lst_formdef_ids) < 1:
+                    new_id = 1
+                else:
+                    new_id = int(sorted(lst_formdef_ids, key=conv)[-1]) + 1
+                lst_formdef_ids.append(new_id)
+                formdef.id = new_id
+                formdef.store()
+            except:
+                print "import error : " + formdef.id
 
