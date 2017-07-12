d7b560e866a66aa0d18c2d02f9e91767bada1bc1
Christophe Boulanger <christophe.boulanger@imio.be>
2017-07-12 11:40:58 +0200

    Passerelle : Add script to add passerelle api user

---
+++ src/scripts/migration-ts1/passerelle/build-api-user.py
@@ -0,0 +1,3 @@
+from passerelle.base.models import ApiUser, AccessRight
+
+api_user = ApiUser.objects.create(username='Tout le monde',fullname='Tout le monde', description='Tout le monde', keytype='', key='')
