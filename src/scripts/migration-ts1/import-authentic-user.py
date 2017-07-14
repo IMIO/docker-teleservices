a8aab84f111bcfdfd219d189b46a8904d1d6e213
Christophe Boulanger <christophe.boulanger@imio.be>
2017-07-14 12:33:50 +0200

    Test if role already exist before create and save them

--- src/scripts/migration-ts1/import-authentic-user.py
+++ src/scripts/migration-ts1/import-authentic-user.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from authentic2.compat import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 from django_rbac.utils import get_role_model, get_ou_model
 from hobo.agent.authentic2.provisionning import provisionning
 import hashlib
@@ -12,17 +13,29 @@ def create_authentic_user():
     organisation_unit = OU.objects.get(default = True)
     with provisionning:
         # create default role in ts2.
-        role_agent_admin = Role(name='Agent administrateur des utilisateurs', ou=organisation_unit)
-        role_agent_admin.save()
+        try:
+            role_agent_admin = Role.objects.get(name='Agent administrateur des utilisateurs')
+        except ObjectDoesNotExist:
+            role_agent_admin = Role(name='Agent administrateur des utilisateurs', ou=organisation_unit)
+            role_agent_admin.save()
 
-        role_agent_fabriques = Role(name='Agent ayant acces aux fabriques', ou=organisation_unit)
-        role_agent_fabriques.save()
+        try:
+            role_agent_fabriques = Role.objects.get(name='Agent ayant acces aux fabriques')
+        except ObjectDoesNotExist:
+            role_agent_fabriques = Role(name='Agent ayant acces aux fabriques', ou=organisation_unit)
+            role_agent_fabriques.save()
 
-        role_agent_traitant_pop = Role(name='Agents traitants - Population, etat civil', ou=organisation_unit)
-        role_agent_traitant_pop.save()
+        try:
+            role_agent_traitant_pop = Role.objects.get(name='Agents traitants - Population, etat civil')
+        except ObjectDoesNotExist:
+            role_agent_traitant_pop = Role(name='Agents traitants - Population, etat civil', ou=organisation_unit)
+            role_agent_traitant_pop.save()
 
-        role_agent_traitant_trav = Role(name='Agents traitants - Travaux', ou=organisation_unit)
-        role_agent_traitant_trav.save()
+        try:
+            role_agent_traitant_trav = Role.objects.get(name='Agents traitants - Travaux')
+        except ObjectDoesNotExist:
+            role_agent_traitant_trav = Role(name='Agents traitants - Travaux', ou=organisation_unit)
+            role_agent_traitant_trav.save()
 
         with open("/tmp/tmp_uuid_agent_admin.txt", 'w') as f:
             f.write(role_agent_admin.uuid)
@@ -36,14 +49,16 @@ def create_authentic_user():
         with open("/tmp/tmp_uuid_agent_traitant_trav.txt", 'w') as f:
             f.write(role_agent_traitant_trav.uuid)
 
-        # Create default user with default organisation unit.
-        user_admin_commune = User(username='admin_commune', ou=organisation_unit)
-        user_admin_commune.set_password(create_password('COMMUNE_ID'))
-        user_admin_commune.save()
-
-        # Set role to user
-        role_agent_admin.members.add(user_admin_commune)
-        role_agent_fabriques.members.add(user_admin_commune)
+        # GET or Create default user with default organisation unit.
+        try:
+            user_admin_commune = User.objects.get(username='admin_commune')
+        except:
+            user_admin_commune = User(username='admin_commune', ou=organisation_unit)
+            user_admin_commune.set_password(create_password('COMMUNE_ID'))
+            user_admin_commune.save()
+            # Set role to user
+            role_agent_admin.members.add(user_admin_commune)
+            role_agent_fabriques.members.add(user_admin_commune)
 
 def create_password(commune_id):
     m = hashlib.md5(commune_id)
