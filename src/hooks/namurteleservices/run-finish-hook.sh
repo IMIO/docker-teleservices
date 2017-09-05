b7b9d9384e52510f1bacece8b851035f2c7e5e35
Christophe Boulanger <christophe.boulanger@imio.be>
2017-09-05 11:04:39 +0200

    Namur : Little modification to add new site-options.cfg in prod and preprod.

--- src/hooks/namurteleservices/run-finish-hook.sh
+++ src/hooks/namurteleservices/run-finish-hook.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # To get extra.js in Backoffice.
 if [ -d "/var/lib/wcs-au-quotidien/namur-formulaires.lescommunes.be" ]; then
-    cp site-option.cfg /var/lib/wcs-au-quotidien/namur-formulaires.lescommunes.be
+    cp site-options-preprod.cfg /var/lib/wcs-au-quotidien/namur-formulaires.lescommunes.be/site-options.cfg
 else
-    cp site-option.cfg /var/lib/wcs-au-quotidien/namur-formulaires.guichet-citoyen.be
+    cp site-option-prod.cfg /var/lib/wcs-au-quotidien/namur-formulaires.guichet-citoyen.be/site-options.cfg
 fi
