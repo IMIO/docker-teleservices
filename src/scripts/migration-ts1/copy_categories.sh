75b0ba2e959cd2be228dd27585d4b42f30ad440e
Christophe Boulanger <christophe.boulanger@imio.be>
2017-09-20 10:22:22 +0200

    WCS-MIGRATION : copy_categories.sh Change /var/lib/wcs-au-quotidien to /var/lib/wcs

--- src/scripts/migration-ts1/copy_categories.sh
+++ src/scripts/migration-ts1/copy_categories.sh
@@ -1,7 +1,7 @@
 # SAMPLE : sh copy_categories.sh lalouviere lescommunes.be
-if ! [ -d /var/lib/wcs-au-quotidien/$1-formulaires.$2/categories ]
+if ! [ -d /var/lib/wcs/$1-formulaires.$2/categories ]
 then
-    (mkdir /var/lib/wcs-au-quotidien/$1-formulaires.$2/categories)
+    (mkdir /var/lib/wcs/$1-formulaires.$2/categories)
 fi
 
-cp categories/* /var/lib/wcs-au-quotidien/$1-formulaires.$2/categories
+cp categories/* /var/lib/wcs/$1-formulaires.$2/categories
