8a2bb361e840223dfaa3be4d74538b3c03adc3ef
Christophe Boulanger <christophe.boulanger@imio.be>
2017-09-20 10:35:31 +0200

    WCS-MIGRATION : Change wcs-au-quotidien to wcs in wcs-scripts

--- src/wcs-scripts/dest_ligne1.py
+++ src/wcs-scripts/dest_ligne1.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
+sys.path.insert(0, '/var/lib/wcs/scripts')
 import dest
 reload(dest)
 result = '%(prenom)s %(nom)s' % dest.get(globals())
--- src/wcs-scripts/dest_ligne2.py
+++ src/wcs-scripts/dest_ligne2.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
+sys.path.insert(0, '/var/lib/wcs/scripts')
 import dest
 reload(dest)
 if dest.get(globals()).get('boite'):
