83422b622352ea8fbbd2ad855d402582edf7ab4e
Christophe Boulanger <christophe.boulanger@imio.be>
2017-09-20 10:57:28 +0200

    WCS-MIGRATION : Namur : hook

--- src/hooks/namurteleservices/run-finish-hook.sh
+++ src/hooks/namurteleservices/run-finish-hook.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # To get extra.js in Backoffice.
-if [ -d "/var/lib/wcs-au-quotidien/namur-formulaires.lescommunes.be" ]; then
-    cp site-options-preprod.cfg /var/lib/wcs-au-quotidien/namur-formulaires.lescommunes.be/site-options.cfg
+if [ -d "/var/lib/wcs/namur-formulaires.lescommunes.be" ]; then
+    cp site-options-preprod.cfg /var/lib/wcs/namur-formulaires.lescommunes.be/site-options.cfg
 else
-    cp site-option-prod.cfg /var/lib/wcs-au-quotidien/namur-formulaires.guichet-citoyen.be/site-options.cfg
+    cp site-option-prod.cfg /var/lib/wcs/namur-formulaires.guichet-citoyen.be/site-options.cfg
 fi
