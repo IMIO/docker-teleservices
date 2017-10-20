d04f8e794b47310ea71f8f9ddd4cfe46c964d8e2
Christophe Boulanger <christophe.boulanger@imio.be>
2017-10-20 13:49:00 +0200

    Namur : change script to generate doc mutiple asking

--- src/wcs-scripts/namur_courrierfaxmail_cpt_documents.py
+++ src/wcs-scripts/namur_courrierfaxmail_cpt_documents.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 DEBUG = False
+# ex_places : Determne ou dans le tableau, on parle de nombre d'exemplaire.
 if DEBUG:
     acte_divorce = {'doc': [['Boul', 'Ch', '07/01/1979', '07/01/2000', '1', '1', ''],
                             ['Boulanger', 'Christophe', '07/01/1979', '07/01/2000', '2', '3', '']], 'ex_places':(4,5)}
@@ -15,18 +16,18 @@ if DEBUG:
     compo_menage = {'doc': [['sdsdf', 'sdfsdf', '10/10/2000', '2', '1', '1']], 'ex_places':(3,4)}
     genealogie = {'doc': [['dfgdfg', 'dfgdfg', '10/10/2010', '2', '2', '1']], 'ex_places':(3,4)}
 else:
-    acte_divorce = {'doc': globals().get("form_var_tab_acte_divorce"), 'ex_places':(4,5)}
-    acte_deces = {'doc': globals().get("form_var_tab_acte_deces"), 'ex_places':(4,5)}
-    acte_mariage = {'doc': globals().get("form_var_tab_acte_mariage"), 'ex_places':(4,5)}
-    acte_naissance = {'doc': globals().get("form_var_tab_acte_naissance"), 'ex_places':(3,4)}
-    certif_cohab_legale = {'doc': globals().get("form_var_tab_cohab_legale"), 'ex_places':(3,4)}
-    certif_nationalite = {'doc': globals().get("form_var_tab_nationalite"), 'ex_places':(3,4)}
-    certif_residence = {'doc': globals().get("form_var_tab_residence"), 'ex_places':(3,4)}
-    certif_residence_natio = {'doc': globals().get("form_var_tab_residence_nationalite"), 'ex_places':(3,4)}
-    certif_residence_histo = {'doc': globals().get("form_var_tab_residence_histo"), 'ex_places':(3,4)}
-    certif_vie = {'doc': globals().get("form_var_tab_certif_vie"), 'ex_places':(3,4)}
-    compo_menage = {'doc': globals().get("form_var_tab_compo_menage"), 'ex_places':(3,4)}
-    genealogie = {'doc': globals().get("form_var_tab_genealogie"), 'ex_places':(3,4)}
+    acte_divorce = {'doc': globals().get("form_var_tab_acte_divorce"), 'ex_places':(4,)}
+    acte_deces = {'doc': globals().get("form_var_tab_acte_deces"), 'ex_places':(4,)}
+    acte_mariage = {'doc': globals().get("form_var_tab_acte_mariage"), 'ex_places':(4,)}
+    acte_naissance = {'doc': globals().get("form_var_tab_acte_naissance"), 'ex_places':(3,)}
+    certif_cohab_legale = {'doc': globals().get("form_var_tab_cohab_legale"), 'ex_places':(3,)}
+    certif_nationalite = {'doc': globals().get("form_var_tab_nationalite"), 'ex_places':(3,)}
+    certif_residence = {'doc': globals().get("form_var_tab_residence"), 'ex_places':(3,)}
+    certif_residence_natio = {'doc': globals().get("form_var_tab_residence_nationalite"), 'ex_places':(3,)}
+    certif_residence_histo = {'doc': globals().get("form_var_tab_residence_histo"), 'ex_places':(3,)}
+    certif_vie = {'doc': globals().get("form_var_tab_certif_vie"), 'ex_places':(3,)}
+    compo_menage = {'doc': globals().get("form_var_tab_compo_menage"), 'ex_places':(3,)}
+    genealogie = {'doc': globals().get("form_var_tab_genealogie"), 'ex_places':(3,)}
 
 documents = {
     'Acte de divorce' : acte_divorce,
@@ -46,8 +47,9 @@ documents = {
 retour = ""
 nom_prenom = ""
 for key, tab_curr_document in documents.items():
-    for row in tab_curr_document['doc']:
-        nom_prenom = "{} {}".format(row[0], row[1])
-        nb_ex_doc = sum(int(row[ex_place]) for ex_place in tab_curr_document['ex_places'])
-        retour = '{}{} copie(s) du formulaire : "{}" pour "{}" \r\n'.format(retour, nb_ex_doc, key, nom_prenom) 
+    if tab_curr_document['doc'] is not None:
+        for row in tab_curr_document['doc']:
+            nom_prenom = "{} {}".format(row[0], row[1])
+            nb_ex_doc = sum(int(row[ex_place]) for ex_place in tab_curr_document['ex_places'])
+            retour = '{}{} copie(s) du formulaire : "{}" pour "{}" {}{}'.format(retour, nb_ex_doc, key, nom_prenom, chr(13), chr(10) )
 result = retour
