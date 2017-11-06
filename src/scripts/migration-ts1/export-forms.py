b2424b09db3c9c2b767551d9ee48e8fdccc0404b
Christophe Boulanger <christophe.boulanger@imio.be>
2017-11-06 14:26:02 +0100

    Update export-forms script

--- src/scripts/migration-ts1/export-forms.py
+++ src/scripts/migration-ts1/export-forms.py
@@ -2,13 +2,13 @@
 
 import os
 import sys
+import xml.etree.ElementTree as ET
 from wcs.formdef import FormDef
+
 for formdef in FormDef.select():
-    json_str = formdef.export_to_json()
-    folder_store_forms = "/var/tmp/{}".format(sys.argv[1])
+    xml_str = ET.tostring(formdef.export_to_xml(include_id=True))
+    folder_store_forms = "/var/lib/wcs/xml_forms_{}".format(sys.argv[1])
     if not os.path.exists(folder_store_forms):
         os.mkdir(folder_store_forms)
-    with open ("{}/{}".format(folder_store_forms, formdef.id), 'w') as myfile:
-        myfile.write(json_str)
-        myfile.close()
-
+    with open ("{}/{}.wcs".format(folder_store_forms, formdef.id), 'w+') as myfile:
+        myfile.write(xml_str)
