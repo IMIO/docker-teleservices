7bbf842861ce4de41cec6110def57a7cd3e05156
Christophe Boulanger <christophe.boulanger@imio.be>
2017-12-12 11:42:13 +0100

    Add new datasources in shell script

--- src/scripts/migration-ts1/copy_datasources.sh
+++ src/scripts/migration-ts1/copy_datasources.sh
@@ -6,3 +6,6 @@ fi
 
 sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/98 > /var/lib/wcs/$1-formulaires.$2/datasources/98
 sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/99 > /var/lib/wcs/$1-formulaires.$2/datasources/99
+sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/100 > /var/lib/wcs/$1-formulaires.$2/datasources/100
+sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/101 > /var/lib/wcs/$1-formulaires.$2/datasources/101
+sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/102 > /var/lib/wcs/$1-formulaires.$2/datasources/102
