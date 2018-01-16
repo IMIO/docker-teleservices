64526969a822d828f704b444c813ad86c131bcf8
Christophe Boulanger <christophe.boulanger@imio.be>
2018-01-16 09:49:08 +0100

    Add new forms and workflows deployments for full version

--- src/scripts/migration-ts1/build-e-guichet.sh
+++ src/scripts/migration-ts1/build-e-guichet.sh
@@ -1,6 +1,6 @@
 # $1 : commune_id (test, local, huy, liege,...)
 # $2 : domain (guichet-citoyen.be, example.net, ...)
-# $3 : Type Instance light ou full
+# $3 : Type Instance light ou full (case sensitive)
 
 # Use postgresql with wcs
 sed -i '/[options] /a postgresql = true' /var/lib/wcs/$1-formulaires.$2/site-options.cfg
@@ -32,20 +32,38 @@ sudo -u passerelle /usr/bin/passerelle-manage tenant_command runscript /opt/publ
 sudo -u passerelle /usr/bin/passerelle-manage tenant_command import_site -d $1-passerelle.$2 /opt/publik/scripts/migration-ts1/passerelle/pays.json
 
 # TODO : voir ce que je peux faire pour les datasources par defaut avec l'url de passerelle hardcodee.
-# TO : voir ce que je peux faire pour la datasource de la liste des pays... autant qu'elle soit dedans ;-).
 
 # Import workflows
 sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/migration-ts1/import-workflows.py /opt/publik/scripts/migration-ts1/workflows/
+if [ $3 = "full" ]
+    then
+    echo "INSTALL WORKFLOWS FOR FULL INSTANCE."
+    sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/migration-ts1/import-workflows.py /opt/publik/scripts/migration-ts1/workflows/only_full/
+fi
 
 # Import forms
 sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/migration-ts1/import-ts1-forms.py /opt/publik/scripts/migration-ts1/forms/
+if [ $3 = "full" ]
+    then
+    echo "INSTALL FORMS FOR FULL INSTANCE."
+    sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/migration-ts1/import-ts1-forms.py /opt/publik/scripts/migration-ts1/forms/only_full/
+fi
 
 # Import combo site structure
-sed -i "s/COMMUNE/$1/g" combo-site/combo-site-structure.json
-sed -i "s/DOMAINE/$2/g" combo-site/combo-site-structure.json
-sudo -u combo combo-manage tenant_command import_site -d $1.$2 /opt/publik/scripts/migration-ts1/combo-site/combo-site-structure.json
-sed -i "s/$1/COMMUNE/g" combo-site/combo-site-structure.json
-sed -i "s/$2/DOMAINE/g" combo-site/combo-site-structure.json
+if [ $3 = "full" ]
+    then
+    sed -i "s/COMMUNE/$1/g" combo-site/combo-site-structure-full.json
+    sed -i "s/DOMAINE/$2/g" combo-site/combo-site-structure-full.json
+    sudo -u combo combo-manage tenant_command import_site -d $1.$2 /opt/publik/scripts/migration-ts1/combo-site/combo-site-structure-full.json
+    sed -i "s/$1/COMMUNE/g" combo-site/combo-site-structure-full.json
+    sed -i "s/$2/DOMAINE/g" combo-site/combo-site-structure-full.json
+else
+    sed -i "s/COMMUNE/$1/g" combo-site/combo-site-structure-light.json
+    sed -i "s/DOMAINE/$2/g" combo-site/combo-site-structure-light.json
+    sudo -u combo combo-manage tenant_command import_site -d $1.$2 /opt/publik/scripts/migration-ts1/combo-site/combo-site-structure-light.json
+    sed -i "s/$1/COMMUNE/g" combo-site/combo-site-structure-light.json
+    sed -i "s/$2/DOMAINE/g" combo-site/combo-site-structure-light.json
+fi
 
 sed -i "s/COMMUNE/$1/g" combo-site/combo-portail-agent-structure.json
 sed -i "s/DOMAINE/$2/g" combo-site/combo-portail-agent-structure.json
