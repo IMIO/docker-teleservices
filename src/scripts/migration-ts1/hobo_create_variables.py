a1aeacb0b8ba17f580fcef6ca23a85fec6060a1e
Christophe Boulanger <christophe.boulanger@imio.be>
2018-02-15 14:52:26 +0100

    Add new script to create global hobo variables

---
+++ src/scripts/migration-ts1/hobo_create_variables.py
@@ -0,0 +1,6 @@
+#usage : sudo -u hobo hobo-manage tenant_command runscript -d demo-hobo.guichet-citoyen.be /opt/publik/scripts/migration-ts1/hobo_create_variables.py
+from hobo.environment.models import Variable
+Variable(name='commune_nom', label='Nom de la commune', value='Ma Commune').save()
+Variable(name='commune_cp', label='Code postal de la commune', value='1111').save()
+Variable(name='commune_rue', label='Rue et n° de la commune', value='1, Rue de ma Commune').save()
+Variable(name='commune_site', label='Site Internet de la commune', value='1, Rue de ma Commune').save()
