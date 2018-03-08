5340fc41615d75d3841429e304d3a9dd43d16520
Christophe Boulanger <christophe.boulanger@imio.be>
2018-03-08 10:01:13 +0100

    change var name and comment in export-workflows

--- src/scripts/migration-ts1/export-workflows.py
+++ src/scripts/migration-ts1/export-workflows.py
@@ -1,4 +1,4 @@
-#USAGE : docker exec -ti [COMMUNE]teleservices_[COMMUNE]teleservices_1 sudo -u  wcs-au-quotidien wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=[COMMUNE]-formulaires.[DOMAIN] /opt/publik/scripts/migration-ts1/export-forms.py [COMMUNE]
+#USAGE : docker exec -ti [COMMUNE]teleservices_[COMMUNE]teleservices_1 sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=[COMMUNE]-formulaires.[DOMAIN] /opt/publik/scripts/migration-ts1/export-workflows.py [COMMUNE]
 
 import os
 import sys
@@ -10,8 +10,8 @@ for wf in Workflow.select():
     xml = wf.export_to_xml(include_id=True)
     misc.indent_xml(xml)
     xml_str = ET.tostring(xml)
-    folder_store_forms = "/var/lib/wcs/xml_wf_{}".format(sys.argv[1])
-    if not os.path.exists(folder_store_forms):
-        os.mkdir(folder_store_forms)
-    with open ("{}/{}.wcs".format(folder_store_forms, formdef.internal_identifier), 'w+') as myfile:
+    folder_store_wf = "/var/lib/wcs/xml_wf_{}".format(sys.argv[1])
+    if not os.path.exists(folder_store_wf):
+        os.mkdir(folder_store_wf)
+    with open ("{}/{}.wcs".format(folder_store_wf, formdef.internal_identifier), 'w+') as myfile:
         myfile.write(xml_str)
