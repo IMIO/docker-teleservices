78d94f66cde1fdf1777e88118d9e61902e7bd4f9
Christophe Boulanger <christophe.boulanger@imio.be>
2018-03-16 13:51:00 +0100

    Namur : new use-case for 'casier-judiciaire

--- src/wcs-scripts/namur_casier.py
+++ src/wcs-scripts/namur_casier.py
@@ -1,4 +1,8 @@
 # Casier judiciaire.
+# use-case : 595
 motif1 = globals().get('form_var_selected_motifs_1')
+# use-case : 596-1
 motif2 = globals().get('form_var_selected_motifs_2')
-result = str(motif1 if motif1 is not None and motif1 != 'None' else motif2)
+# use-case : 596-2
+motif3 = globals().get('form_var_selected_motifs_3')
+result = str(motif1 if motif1 is not None and motif1 != 'None' else motif2 if motif2 is not None and motif2 != 'None' else motif3)
