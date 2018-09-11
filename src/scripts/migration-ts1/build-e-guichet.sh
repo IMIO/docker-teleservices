02c7a046670ee337e3e06462abef57096f6902e9
Christophe Boulanger <christophe.boulanger@imio.be>
2018-09-11 16:33:35 +0200

    update path to only_full forms

--- src/scripts/migration-ts1/build-e-guichet.sh
+++ src/scripts/migration-ts1/build-e-guichet.sh
@@ -47,9 +47,9 @@ fi
 if [ $3 = "full" ]
     then
     echo "INSTALL FORMS FOR FULL INSTANCE."
-    sed -i "s/[cp_commune]/$4/g" /opt/publik/scripts/migration-ts1/forms/only_full/2018_04_updates/*.wcs
-    sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/migration-ts1/import-forms.py /opt/publik/scripts/migration-ts1/forms/only_full/2018_04_updates/
-    sed -i "s/$4/[cp_commune]/g" /opt/publik/scripts/migration-ts1/forms/only_full/2018_04_updates/*.wcs
+    sed -i "s/[cp_commune]/$4/g" /opt/publik/scripts/migration-ts1/forms/only_full/*.wcs
+    sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/migration-ts1/import-forms.py /opt/publik/scripts/migration-ts1/forms/only_full/
+    sed -i "s/$4/[cp_commune]/g" /opt/publik/scripts/migration-ts1/forms/only_full/*.wcs
     sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/migration-ts1/import-forms.py /opt/publik/scripts/migration-ts1/forms/models/
 else
     echo "INSTALL FORMS FOR LIGHT INSTANCE."
