676d8bebdf1835f1b2245a8e1146d7c20ab124b9
Christophe Boulanger <christophe.boulanger@imio.be>
2018-10-08 09:08:08 +0200

    Register admin permissions when 'build e guichet'

--- src/scripts/build-e-guichet/import-permissions.py
+++ src/scripts/build-e-guichet/import-permissions.py
@@ -19,6 +19,14 @@ with open("/tmp/tmp_uuid_agent_fabriques.txt", 'r') as file_aa:
         permissions['workflows'].append(uuid_aa)
     file_aa.close()
 
+with open("/tmp/tmp_uuid_debug.txt", 'r') as file_debug:
+    uuid_debug = file_debug.read()
+    permissions['forms'].append(uuid_debug)
+    permissions['categories'].append(uuid_debug)
+    permissions['workflows'].append(uuid_debug)
+    permissions['settings'].append(uuid_debug)
+    permissions['bounces'].append(uuid_debug)
+
 pub.cfg['admin-permissions'] = permissions
 pub.write_cfg()
 
