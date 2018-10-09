f562ef9b8bbc430c8ab3b4af2d27903a4ebe7a3c
Christophe Boulanger <christophe.boulanger@imio.be>
2018-10-09 13:14:14 +0200

    Update script to load default wcs config (database,...)

--- src/scripts/build-e-guichet/wcs_config.py
+++ src/scripts/build-e-guichet/wcs_config.py
@@ -8,14 +8,16 @@ from quixote import get_publisher
 pub = get_publisher()
 pub.cfg['misc']['homepage-redirect-url'] = 'https://{0}.guichet-citoyen.be/demarches/'.format(sys.argv[1])
 
-pub.cfg['postgresql']['database'] = 'teleservices_{0}_wcs'.format(sys.argv[1])
-pub.cfg['postgresql']['user'] = 'teleservices_{0}_teleservices'.format(sys.argv[1])
-pub.cfg['postgresql']['host'] = 'database.lan.imio.be'
-pub.cfg['postgresql']['port'] = '5432'
 with open('/etc/combo/settings.py') as f:
     for line in f:
         if "DATABASES['default']['PASSWORD']" in line:
-            password = line.split(' = ')[1].translate(None, "'")
-pub.cfg['postgresql']['password'] = password
+            password = line.split(' = ')[1].translate(None, "'").replace('\n','')
+pub.cfg['postgresql'] = {'database':'teleservices_{0}_wcs'.format(sys.argv[1]),
+                        'user':'teleservices_{0}_teleservices'.format(sys.argv[1]),
+                        'host':'database.lan.imio.be',
+                        'port':'5432',
+                        'password':password
+        }
+
 pub.cfg['emails']['smtp_server'] = 'mailrelay.imio.be'
 pub.write_cfg()
