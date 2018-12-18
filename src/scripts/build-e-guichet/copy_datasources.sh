a83270b324800c6678258b389aaafe6f57f1c5f8
Christophe Boulanger <christophe.boulanger@imio.be>
2018-12-18 11:36:16 +0100

    Datasources / categories : Add command to change folder owner

--- src/scripts/build-e-guichet/copy_datasources.sh
+++ src/scripts/build-e-guichet/copy_datasources.sh
@@ -1,7 +1,7 @@
 # SAMPLE : sh copy_categories.sh lalouviere lescommunes.be
 if ! [ -d /var/lib/wcs/$1-formulaires.$2/datasources ]
 then
-    (mkdir /var/lib/wcs/$1-formulaires.$2/datasources)
+    (mkdir /var/lib/wcs/$1-formulaires.$2/datasources && chown wcs:wcs /var/lib/wcs/$1-formulaires.$2/datasources -Rf)
 fi
 
 sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/98 > /var/lib/wcs/$1-formulaires.$2/datasources/98
