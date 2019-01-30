cd860003b1eba58979cf849e62ab3a87b59f4968
Christophe Boulanger <christophe.boulanger@imio.be>
2019-01-30 08:21:03 +0100

    Add passerelle module in settings.d (fees and ts1datasources)

---
+++ config/passerelle/settings.d/ts1datassources.py
@@ -0,0 +1,5 @@
+if 'passerelle_imio_ts1_datasources' not in INSTALLED_APPS:
+    INSTALLED_APPS += ('passerelle_imio_ts1_datasources',)
+    TENANT_APPS += ('passerelle_imio_ts1_datasources',)
+    PASSERELLE_APP_PASSERELLE_IMIO_TS1_DATASOURCES_ENABLED = True
+
