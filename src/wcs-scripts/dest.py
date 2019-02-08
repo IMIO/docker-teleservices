a1c8258cc8b4274d4823e5e724299249d01b1f1d
Christophe Boulanger <christophe.boulanger@imio.be>
2019-02-08 15:28:27 +0100

    dest can now works without names suffix.

--- src/wcs-scripts/dest.py
+++ src/wcs-scripts/dest.py
@@ -4,13 +4,16 @@ def get(variables):
     if acceptation in ('Simple', 'Gratuit'):
         liste_vars = ['demandeur_liegeois', 'demandeur_plus_lg', 'demandeur',
                 'demandeur_rad', 'demandeur_new_lg_etranger',
-                'demandeur_new_lg_belgique']
+                'demandeur_new_lg_belgique', '']
     elif acceptation in ('Tiers', ):
         liste_vars = ['perscon', 'perscon_plus_lg']
     result = {}
     for attribute in ('prenom', 'nom', 'rue', 'complement_adresse', 'numero', 'boite', 'codepostal', 'localite', 'pays'):
         for var in liste_vars:
-            varname = 'form_var_%s_%s' % (attribute, var)
+            if var != '':
+                varname = 'form_var_%s_%s' % (attribute, var)
+            else:
+                varname = 'form_var_%s' % (attribute)
             result[attribute] = variables.get(varname) or result.get(attribute) or ''
             if result[attribute] == 'None':
                 result[attribute] = ''
