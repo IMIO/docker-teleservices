412ba3de9685ac97dfe5872a026664b47af8e9e8
Christophe Boulanger <christophe.boulanger@imio.be>
2019-03-08 11:49:46 +0100

    use new debug var to test town-time forms whithout checking duplicates nrn

--- src/wcs-scripts/check_duplicate_nn.py
+++ src/wcs-scripts/check_duplicate_nn.py
@@ -1,2 +1,6 @@
-field_id = [x for x in form_objects.formdef.fields if x.varname == 'nn'][0].id
-result = bool(len([x for x in form_objects.formdef.data_class().select() if x.data.get(field_id) == form_var_nn and not x.is_draft()]) == 0)
+# create hobo variable "check_duplicate_nn" to test forms without checking duplicate nn in DB.
+if globals().get('check_duplicate_nn') and str(globals().get('check_duplicate_nn')) == 'False':
+    result = False
+else:
+    field_id = [x for x in form_objects.formdef.fields if x.varname == 'nn'][0].id
+    result = bool(len([x for x in form_objects.formdef.data_class().select() if x.data.get(field_id) == form_var_nn and not x.is_draft()]) == 0)
