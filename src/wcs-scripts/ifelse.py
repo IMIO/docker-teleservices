a3bfb65831df2de9bbbd8c158780f0d21e9acde0
Christophe Boulanger <christophe.boulanger@imio.be>
2019-04-05 13:53:17 +0200

    Lalouviere : ifelse : repas.

--- src/wcs-scripts/ifelse.py
+++ src/wcs-scripts/ifelse.py
@@ -1,4 +1,6 @@
 def ifelse(*args):
-    return args[0] or args[1]
-result = str(ifelse(*args))
+    arg0 = globals().get(args[0])
+    arg1 = globals().get(args[1]) or args[1]
+    return arg0 or arg1
 
+result = str(ifelse(*args))
