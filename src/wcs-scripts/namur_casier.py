df5076be7767a7dd834ccfc8915a9172a27d3144
Christophe Boulanger <christophe.boulanger@imio.be>
2019-04-12 13:25:54 +0200

    Namur : correction to generate new tab if user change the reason of the request in 'Ext. casier judiciaire'.

--- src/wcs-scripts/namur_casier.py
+++ src/wcs-scripts/namur_casier.py
@@ -1,8 +1,11 @@
 # Casier judiciaire.
-# use-case : 595
-motif1 = globals().get('form_var_selected_motifs_1')
-# use-case : 596-1
-motif2 = globals().get('form_var_selected_motifs_2')
-# use-case : 596-2
-motif3 = globals().get('form_var_selected_motifs_3')
-result = str(motif1 if motif1 is not None and motif1 != 'None' else motif2 if motif2 is not None and motif2 != 'None' else motif3)
+raison = globals().get('form_var_raison_demande') or ''
+motif = ''
+if '595' in raison:
+    motif = globals().get('form_var_selected_motifs_1')
+if '596-1' in raison:
+    motif = globals().get('form_var_selected_motifs_2')
+if '596-2' in raison:
+    motif = globals().get('form_var_selected_motifs_3')
+
+result = str(motif)
