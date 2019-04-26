fb401455cc54d54af96b4e65609fe059d2aa5181
Christophe Boulanger <christophe.boulanger@imio.be>
2019-04-26 15:20:25 +0200

    some change to get similar observations

--- src/wcs-scripts/get_similars_observations.py
+++ src/wcs-scripts/get_similars_observations.py
@@ -13,14 +13,29 @@ if os.path.dirname(__file__) not in sys.path:
 import close_demands
 
 result = ''
+list_sous_domaines = ['var_sous_domaine_proprete',
+        'var_sous_domaine_avaloir',
+        'var_sous_domaine_voirie',
+        'var_sous_domaine_graffiti',
+        'var_sous_domaine_espace_vert',
+        'var_sous_domaine_signalisation',
+        'var_sous_domaine_mobilier_urbain',
+        'var_sous_domaine_voirie'
+        ]
+
 
 def get_similars_observations_count(data):
     coords = close_demands.get_coords(data)
-    result = "0"
+    result = 0
     if coords:
-        lst_similar_forms = list(close_demands.get_close_demands(form_objects.formdef, coords, vars()))
-        result = str(len(lst_similar_forms))
-    return result
+        for item in close_demands.get_close_demands(form_objects.formdef, coords, data):
+            structured_item =  item.get_as_dict()
+            if structured_item['var_incident'] == form_var_incident:
+                for sous_domaine in list_sous_domaines:
+                    if structured_item.has_key(sous_domaine):
+                        if structured_item[sous_domaine] is not None and structured_item[sous_domaine] == data.get('form_{}'.format(sous_domaine)):
+                            result = result + 1
+    return str(result)
 
 #form_status : contient le status courant de la demande.
 def get_similars_observations_mails(data):
@@ -28,15 +43,15 @@ def get_similars_observations_mails(data):
     lst_mails = []
     result = ''
     if coords:
-        lst_similar_forms = list(close_demands.get_close_demands(form_objects.formdef, coords, vars()))
-        if len(lst_similar_forms) > 0:
-            formdef = lst_similar_forms[0].formdef
-            formdatas = formdef.data_class().select(lambda x: x.id_display!=form_number)
-            for formdata in formdatas:
-                for field in formdef.get_all_fields():
-                    # possiblement, faire evoluer le wf de la demande courante jusqu'au statut dans lequel est la 1ere demande? Sauf si cette demande est deja plus loin dans le wf?
-                    if field.varname is not None and  "mail_for_similar_observation" in field.varname:
-                        lst_mails.append(formdata.get_field_view_value(field))
+        for item in close_demands.get_close_demands(form_objects.formdef, coords, data):
+            structured_item =  item.get_as_dict()
+            if structured_item['var_incident'] == form_var_incident:
+                for sous_domaine in list_sous_domaines:
+                    if structured_item.has_key(sous_domaine):
+                        if structured_item[sous_domaine] is not None and structured_item[sous_domaine] == data.get('form_{}'.format(sous_domaine)):
+                            if structured_item.has_key('var_mail_for_similar_observation') and 
+                               structured_item['var_mail_for_similar_observation'] is not None:
+                                lst_mails.append(formdata.get_field_view_value(field))
         result = ','.join(lst_mails)
     return result
 
