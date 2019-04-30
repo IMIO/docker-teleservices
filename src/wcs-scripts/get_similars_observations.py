352332b0c29d8d58a49b02771bd24e407e928976
Christophe Boulanger <christophe.boulanger@imio.be>
2019-04-30 16:34:40 +0200

    Change method to get similar observation (with PBLV)

--- src/wcs-scripts/get_similars_observations.py
+++ src/wcs-scripts/get_similars_observations.py
@@ -4,6 +4,9 @@
 # similar_list.py  
 # similar_map.py
 
+#usage on a form  : str(int(script.get_similars_observations('count')) + 1)
+
+
 import os
 import sys
 
@@ -55,7 +58,40 @@ def get_similars_observations_mails(data):
         result = ','.join(lst_mails)
     return result
 
+def set_data_on_first_observation(data, signalement_similaire):
+    # Set first observation on current demand.
+    # form_var_first_observation = signalement_similaire
+    formdef = form_objects.formdef
+    cpt_signalements = 0
+    for formdata in formdef.data_class().select():
+        # structured_item =  formdata.get_as_dict()
+        tmp_form_number = max([int(i) for i in formdata.keys()])
+        if formdata.id == signalement_similaire:
+            for field in formdef.get_all_fields():
+                if field.varname is not None:
+                    if 'str_all_mails' == field.varname and data.get('form_var_mail_for_similar_observation','') != '':
+                        if field.id not in formdata.data.keys():
+                            formdata.data[field.id] = form_var_mail_for_similar_observation
+                        else:
+                            formdata.data[field.id] = "{},{}".format(formdata.data[field.id], form_var_mail_for_similar_observation)
+                    if 'signalements' == field.varname:
+                        tmp_form_number = max([int(i) for i in formdata.keys()])
+                        if field.id not in formdata.data.keys():
+                            formdata.data[field.id] = tmp_form_number
+                            cpt_signalements = 1
+                        else:
+                            lst_signalements = formdata.data[field.id].split(',')
+                            if str(tmp_form_number) not in lst_signalements:
+                                formdata.data[field.id] = "{},{}".format(formdata.data[field.id], tmp_form_number)
+                            cpt_signalements = len(lst_signalements) 
+                    if 'cpt_signalements' == field.varname:
+                        formdata.data[field.id] = str(cpt_signalements)
+                formdata.store()
+    return str(signalement_similaire)
+
 if args[0] == "mail":
     result = get_similars_observations_mails(vars())
 if args[0] == "count":
     result = get_similars_observations_count(vars())
+if args[0] == "set_on_first_observation":
+    result = set_data_on_first_observation(vars(), args[1])
