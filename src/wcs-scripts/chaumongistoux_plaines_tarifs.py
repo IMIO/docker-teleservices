65b76a3a45539f7273c9d73c5754913a5782b793
Christophe Boulanger <christophe.boulanger@imio.be>
2019-05-06 09:56:10 +0200

    Chaumon Gistoux : update scripts (compute)

--- src/wcs-scripts/chaumongistoux_plaines_tarifs.py
+++ src/wcs-scripts/chaumongistoux_plaines_tarifs.py
@@ -1,13 +1,21 @@
+# -*- coding: utf-8 -*-
+from decimal import Decimal
+import os
+import sys
 
-def cout_accueil():
-    return str(Decimal(form_option_cout_garderie) * (len((form_var_garderie0107_0507_raw if "du 1er juillet au 5 juillet 2019" in form_var_semainestage else [])                                       + (form_var_garderie0807_1207_raw if "du 8 juillet au 12 juillet 2019" in form_var_semainestage else [])                                       + (form_var_garderie1507_1907_raw if "du 15 juillet au 19 juillet 2019" in form_var_semainestage else [])                                       + (form_var_garderie2207_2607_raw if "du 22 juillet au 26 juillet 2019" in form_var_semainestage else [])                                       + (form_var_garderie2907_0208_raw if "du 29 juillet au 2 août 2019" in form_var_semainestage else [])                                       + (form_var_garderie0508_0908_raw if "du 5 août au 9 août 2019" in form_var_semainestage else []))))
+
+if os.path.dirname(__file__) not in sys.path:
+    sys.path.append(os.path.dirname(__file__))
 
 
-def cout_reservation():
-    return str(Decimal(form_option_cout_resident if form_var_code_postal == "1325" else form_option_cout_non_resident) * len(form_var_semainestage_raw))
+def cout_accueil(data):
+    return str(Decimal(data.get('form_option_cout_garderie')) * len((data.get('form_var_garderie_s1_raw') or [] if "du 1er juillet au 5 juillet 2019" in data.get('form_var_semainestage') else []) + (data.get('form_var_garderie_s2_raw') or [] if "du 8 juillet au 12 juillet 2019" in data.get('form_var_semainestage') else []) + (data.get('form_var_garderie_s3_raw') or [] if "du 15 juillet au 19 juillet 2019" in data.get('form_var_semainestage') else []) + (data.get('form_var_garderie_s4_raw') or [] if "du 22 juillet au 26 juillet 2019" in dataa.get('form_var_semainestage') else []) + (data.get('form_var_garderie_s5_raw') or [] if "du 29 juillet au 2 août 2019" in data.get('form_var_semainestage') else []) + (data.get('form_var_garderie_s6_raw') or [] if "du 5 août au 9 août 2019" in data.get('form_var_semainestage') else [])))
 
-if args[0] == "cout_accueil":
-    result = cout_accueil
+def cout_reservation(data):
+    return str(Decimal(data.get('form_option_cout_resident') if data.get('form_var_code_postal') == "1325" else data.get('form_option_cout_non_resident')) * len(data.get('form_var_semainestage_raw')))
 
-if args[0] == "cout_reservation":
-    result = cout_reservation
+if args[0] == "coutaccueil":
+    result = cout_accueil(vars())
+
+if args[0] == "coutreservation":
+    result = cout_reservation(vars())
