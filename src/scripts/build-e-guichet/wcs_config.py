e1d93fd80d032c4767ad3bfd041940c4071fc8c6
Christophe Boulanger <christophe.boulanger@imio.be>
2019-07-17 13:45:01 +0200

    file settings.d/settings.py doesn't exist in local deployment...

--- src/scripts/build-e-guichet/wcs_config.py
+++ src/scripts/build-e-guichet/wcs_config.py
@@ -7,17 +7,22 @@ from quixote import get_publisher
 
 pub = get_publisher()
 pub.cfg['misc']['homepage-redirect-url'] = 'https://{0}.guichet-citoyen.be/demarches/'.format(sys.argv[1])
-
-with open('/etc/combo/settings.d/settings.py') as f:
-    for line in f:
-        if "DATABASES['default']['PASSWORD']" in line:
-            password = line.split(' = ')[1].translate(None, "'").replace('\n','')
-pub.cfg['postgresql'] = {'database':'teleservices_{0}_wcs'.format(sys.argv[1]),
+try:
+    with open('/etc/combo/settings.d/settings.py') as f:
+        for line in f:
+            if "DATABASES['default']['PASSWORD']" in line:
+                password = line.split(' = ')[1].translate(None, "'").replace('\n','')
+    pub.cfg['postgresql'] = {'database':'teleservices_{0}_wcs'.format(sys.argv[1]),
                         'user':'teleservices_{0}_teleservices'.format(sys.argv[1]),
                         'host':'database.lan.imio.be',
                         'port':'5432',
-                        'password':password
-        }
-
+                        'password':password}
+except:
+    pub.cfg['postgresql'] = {'database':'wcs',
+                        'user':'postgres',
+                        'host':'database',
+                        'port':'5432',
+                        'password':'password'}
+    
 pub.cfg['emails']['smtp_server'] = 'mailrelay.imio.be'
 pub.write_cfg()
