98a26e42a7cce951cc6f61f89c1b84490c8d5bc9
Christophe Boulanger <christophe.boulanger@imio.be>
2019-12-06 11:00:03 +0100

    update extraction scripts for "La Louvière"

--- src/hooks/lalouviereteleservices/extra_scolaire_csv.py
+++ src/hooks/lalouviereteleservices/extra_scolaire_csv.py
@@ -6,37 +6,63 @@
 import csv
 import re
 import sys
-from time import mktime
 from datetime import datetime
+from time import mktime
+
 from wcs.formdef import FormDef
 
 dicforms = {
-    'tickets_repas':'Commande de tickets repas',
-    'cartes_garderies':'Commande de cartes de garderie'
+    "tickets_repas": "Commande de tickets repas",
+    "cartes_garderies": "Commande de cartes de garderie",
 }
 
-for formdef in FormDef.select(lambda x: x.name== dicforms[sys.argv[1]]):
+for formdef in FormDef.select(lambda x: x.name == dicforms[sys.argv[1]]):
     for formdata in formdef.data_class().select():
         r = re.compile("Paiement effectu.*")
-        if len(filter(r.match, [evolution.get_status().name for evolution in formdata.evolution])) > 0:
+        if (
+            len(
+                filter(
+                    r.match,
+                    [evolution.get_status().name for evolution in formdata.evolution],
+                )
+            )
+            > 0
+        ):
             columns = []
-            columns.append(formdata.get_display_id()) # identifiant du formulaire
+            columns.append(formdata.get_display_id())  # identifiant du formulaire
             creation_date = datetime.fromtimestamp(mktime(formdata.receipt_time))
-            columns.append(creation_date.strftime('%d/%m/%Y %H:%M:%S'))
+            columns.append(creation_date.strftime("%d/%m/%Y %H:%M:%S"))
             last_update_date = datetime.fromtimestamp(mktime(formdata.last_update_time))
-            columns.append(last_update_date.strftime('%d/%m/%Y %H:%M:%S'))
-            columns.append(formdata.user.get_display_name() if formdata.user else '-')
-            for field in formdef.get_all_fields(): # les autres champs
-                if not hasattr(field, 'get_view_value'): # sauf les titres, etc.
+            columns.append(last_update_date.strftime("%d/%m/%Y %H:%M:%S"))
+            columns.append(formdata.user.get_display_name() if formdata.user else "-")
+            for field in formdef.get_all_fields():  # les autres champs
+                if not hasattr(field, "get_view_value"):  # sauf les titres, etc.
                     continue
-                if field.in_listing or field.varname == 'nom_eleve' or field.varname == 'prenom_eleve':
-                    field_value = formdata.get_field_view_value(field) or ''
-                    if hasattr(field,'get_display_value') and field.get_display_value(formdata.get_field_view_value(field)) is not None:
-                        field_value = field.get_display_value(formdata.get_field_view_value(field))
+                if hasattr(field, "in_listing") and field.in_listing:
+                    field_value = formdata.get_field_view_value(field) or ""
+                if hasattr(field, "include_in_listing") and (
+                    field.include_in_listing
+                    or field.varname == "nom_eleve"
+                    or field.varname == "prenom_eleve"
+                ):
+                    field_value = formdata.get_field_view_value(field) or ""
+                    if (
+                        hasattr(field, "get_display_value")
+                        and field.get_display_value(
+                            formdata.get_field_view_value(field)
+                        )
+                        is not None
+                    ):
+                        field_value = field.get_display_value(
+                            formdata.get_field_view_value(field)
+                        )
                     columns.append(field_value)
 
-            with open('/var/tmp/{}.csv'.format(sys.argv[1]), 'a') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter='|',
-                                quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+            with open("/var/tmp/{}.csv".format(sys.argv[1]), "a") as csvfile:
+                csvwriter = csv.writer(
+                    csvfile,
+                    delimiter="|",
+                    quoting=csv.QUOTE_MINIMAL,
+                    lineterminator="\n",
+                )
                 csvwriter.writerow(columns)
-
