bea6c2c31948c8cdb09da7431c8a45f693654200
Frédéric Péters <fpeters@entrouvert.com>
2020-04-22 11:09:21 +0200

    wcs scripts: get reload() from importlib module (#41969)

--- src/wcs-scripts/dest_ligne1.py
+++ src/wcs-scripts/dest_ligne1.py
@@ -1,4 +1,8 @@
 import sys
+try:
+    from importlib import reload
+except ImportError:
+    pass
 sys.path.insert(0, '/var/lib/wcs/scripts')
 sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
 import dest
