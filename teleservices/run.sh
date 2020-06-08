5d7c4357d276b68f9b02ac4b967e46d927fb5968
Daniel Muyshond <33881615+dmshd@users.noreply.github.com>
2020-06-08 17:36:48 +0200

    fix uploads & attchmts permission check path

--- teleservices/run.sh
+++ teleservices/run.sh
@@ -13,8 +13,8 @@ chown passerelle:passerelle /var/lib/passerelle/tenants -R
 chown wcs:wcs /var/lib/wcs -R
 
 # uploads & attchmts permission check
-chown -R wcs:wcs attachments/ 
-chown -R wcs:wcs uploads/
+chown -R wcs:wcs /var/lib/wcs/*/attachments/ 
+chown -R wcs:wcs var/lib/wcs/*/uploads/
 
 python /var/lib/authentic2/locale/fr/LC_MESSAGES/mail-translation.py
 test -e /var/lib/wcs/configure-wcs.py && python /var/lib/wcs/configure-wcs.py
