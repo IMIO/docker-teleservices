38834ab7e53c09f855670900ac7bf3ecf2ee166c
Nicolas Hislaire <nicolas.hislaire@imio.be>
2022-07-01 09:58:14 +0200

    [TELE-1337] try to redispatch crons

--- teleservices/run.sh
+++ teleservices/run.sh
@@ -37,19 +37,19 @@ test -f /opt/publik/hooks/$HOSTNAME/run-hook.sh && /opt/publik/hooks/$HOSTNAME/r
 
 # alter authentic jobs to run at random time
 sed -i "s/minute=0/minute=$(( ( RANDOM % 5 ) ))/" /etc/authentic2-multitenant/authentic2-multitenant-uwsgi.ini
-sed -i "s/minute=5/minute=$(( 6 + ( RANDOM % 5 ) ))/" /etc/authentic2-multitenant/authentic2-multitenant-uwsgi.ini
-sed -i "s/minute=15/minute=$(( 15 + ( RANDOM % 10 ) ))/" /etc/authentic2-multitenant/authentic2-multitenant-uwsgi.ini
+sed -i "s/minute=5/minute=$(( 6 + ( RANDOM % 10 ) ))/" /etc/authentic2-multitenant/authentic2-multitenant-uwsgi.ini
+sed -i "s/minute=15/minute=$(( 20 + ( RANDOM % 20 ) ))/" /etc/authentic2-multitenant/authentic2-multitenant-uwsgi.ini
 
 # alter bijoe job to run at random time during the night
 cp /opt/publik/scripts/scripts_teleservices/bijoe_cron_randomizer/bijoe_new_cron /etc/cron.d/bijoe_random
 
 # alter chrono jobs to run at random time
 sed -i "s/unique-cron = -5/unique-cron = -$(( 5 + ( RANDOM % 6 ) ))/" /etc/chrono/uwsgi.ini
-sed -i "s/unique-cron = 1 -1/unique-cron = $(( ( RANDOM % 60 ) )) -1/" /etc/chrono/uwsgi.ini
+sed -i "s/unique-cron = 1 -1/unique-cron = $(( 15 + ( RANDOM % 40 ) )) -1/" /etc/chrono/uwsgi.ini
 sed -i "s/unique-cron = 2 4/unique-cron = $(( (RANDOM % 60 ) )) $((2 + (RANDOM % 3) ))/" /etc/chrono/uwsgi.ini
 
 # alter combo jobs to run at random time
-sed -i "s/0 8/$(( ( RANDOM % 60 ) )) $(( 7 + ( RANDOM % 3 ) ))/" /etc/cron.d/combo
+sed -i "s/0 8/$(( ( RANDOM % 60 ) )) $(( 6 + ( RANDOM % 6 ) ))/" /etc/cron.d/combo
 sed -i "s#\*/10#$(( ( RANDOM % 9 ) ))-59/10#" /etc/cron.d/combo
 
 # alter hobo job to run at random time
@@ -57,8 +57,8 @@ sed -i "s/50/$(( 40 + ( RANDOM % 16 ) ))/" /etc/cron.d/hobo-agent
 
 # alter passerelle jobs to run at random time
 sed -i "s/cron = -5/cron = -$(( 5 + ( RANDOM % 6 ) ))/" /etc/passerelle/uwsgi.ini
-sed -i "s/cron = 1 -1/cron = $(( ( RANDOM % 6 ) )) -1/" /etc/passerelle/uwsgi.ini
-sed -i "s/cron = 17 -1/cron = $(( ( RANDOM % 60 ) )) -1/" /etc/passerelle/uwsgi.ini
+sed -i "s/cron = 1 -1/cron = $(( 6 + ( RANDOM % 10 ) )) -1/" /etc/passerelle/uwsgi.ini
+sed -i "s/cron = 17 -1/cron = $(( 17 + ( RANDOM % 40 ) )) -1/" /etc/passerelle/uwsgi.ini
 
 # Check if UTF8 is well configured (wcs cron jobs)
 if ! grep -q 'LANG=C.UTF-8' /etc/cron.d/wcs; then
