5b5af5ab449eb57abba5028d2574dd6b3dddc14a
Daniel Muyshond <daniel.muyshond@imio.be>
2023-09-05 09:56:20 +0200

    [SUP-31982] Make cboulanger script even more maintainable

--- teleservices/hooks/lalouviereteleservices/extra_scolaire_csv.py
+++ teleservices/hooks/lalouviereteleservices/extra_scolaire_csv.py
@@ -1,8 +1,6 @@
-# Test script WCS to export 1 specific form to "csv"
-# sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2
-# sudo -u wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=lalouviere-formulaires.guichet-citoyen.be extra_scolaire_csv.py
-# arg : tickets_repas or cartes_garderies
-
+"""
+This will export some data from a specific form to a csv file.
+"""
 import csv
 import os
 import re
@@ -17,104 +15,138 @@ FORMS_DICT = {
     "tickets_repas": "Commande de tickets repas",
     "cartes_garderies": "Commande de cartes de garderie",
 }
-FILENAME = sys.argv[1]
-FORM_FULLNAME = FORMS_DICT[FILENAME]
-FILE_OPEN_METHOD = "w+"
-SAVE_FILE_PATH = f"/var/tmp/{FILENAME}.csv"
-
-# Verify if there is an argument or provide help
-if len(sys.argv) < 2:
-    print("Please provide an argument")
-    print("arg : 'tickets_repas' or 'cartes_garderies'")
-    sys.exit()
-
-if path.exists(SAVE_FILE_PATH):
-    os.remove(SAVE_FILE_PATH)
 
 
-for form_def in FormDef.select(lambda x: x.name == FORM_FULLNAME):
-    forms_data_full_list = form_def.data_class().select()
-    for form_data in forms_data_full_list:
-        # Status to filter :
-        r = re.compile("Paiement effectu.*")
-        # Checks if the creation_date is after 2019 and at least one name in
-        # form_data.evolution matches the regular expression r
-        # Convert receipt_time to datetime object
-        creation_date = datetime.fromtimestamp(mktime(form_data.receipt_time))
+def main():
+    """
+    This will export some data from a specific form to a csv file.
+    Initially written by cboulanger, it was not working due to a bug in the
+    evolution.get_status().name part (see SUP-31982 and below for more details)
 
-        # This is initial code written by cboulanger. It's currently not working
-        # since evolution.get_status().name is None on some elements. It blocks
-        # the script from running and accomplishing its task.
-        # I let it here for reference.
-        #
-        # creation_date = datetime.fromtimestamp(mktime(form_data.receipt_time))
-        # if (
-        #     creation_date.year > 2019
-        #     and sum(
-        #         1
-        #         for _ in filter(
-        #             r.match,
-        #             [evolution.get_status().name for evolution in form_data.evolution],
-        #         )
-        #     )
-        #     > 0
-        # ):
+    Args (wcsctl):
+        $1: vhost
+        $2: domain
 
-        # Unlock solution starts here
-        # This is a attempt to unlock the script. It's not working either with the
-        # previous commenter part (see explanations above).
-        if creation_date.year > 2019:
-            status_names = [
-                status.name
-                for evolution in form_data.evolution
-                if (status := evolution.get_status()) is not None
-                and status.name is not None
-            ]
+    Args (script):
+        1: form name (tickets_repas or cartes_garderies)
 
-            matching_count = sum(1 for _ in filter(r.match, status_names))
+    Example:
+    sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript \
+        --vhost=$1-formulaires.$2
+    sudo -u wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript \
+        --vhost=lalouviere-formulaires.guichet-citoyen.be extra_scolaire_csv.py
+    """
+    # pylint: disable-msg=too-many-locals
 
-            if matching_count > 0:
-                columns = []
-                columns.append(form_data.get_display_id())  # identifiant du formulaire
-                columns.append(creation_date.strftime("%d/%m/%Y %H:%M:%S"))
-                last_update_date = datetime.fromtimestamp(
-                    mktime(form_data.last_update_time)
+    # Verify if there is an argument or provide help (print docstring)
+    if len(sys.argv) < 2:
+        print("Hey, this needs an argument to work!\n")
+        print("Here is the main.__doc__:\n")
+        print(main.__doc__)
+        sys.exit()
+
+    filename = sys.argv[1]
+    form_fullname = FORMS_DICT[filename]
+    file_open_method = "w+"
+    save_file_path = f"/var/tmp/{filename}.csv"
+
+    if path.exists(save_file_path):
+        os.remove(save_file_path)
+
+    for form_def in FormDef.select(lambda x: x.name == form_fullname):
+        forms_data_full_list = form_def.data_class().select()
+        for form_data in forms_data_full_list:
+            # Check for the presente of "Paiement effectué" in form evolution
+            status_to_filter_regex = re.compile("Paiement effectu.*")
+
+            # Check if the creation_date is after 2019 and at least one name in
+            # form_data.evolution matches the regular expression status_to_filter_regex
+
+            # Retrieve creation date convertin receipt_time to datetime object
+            creation_date = datetime.fromtimestamp(mktime(form_data.receipt_time))
+
+            # This is initial code written by cboulanger. It's currently not working
+            # since evolution.get_status().name is None on some elements. It blocks
+            # the script from running and accomplishing its task.
+            # I let it here for reference.
+            #
+            # creation_date = datetime.fromtimestamp(mktime(form_data.receipt_time))
+            # if (
+            #     creation_date.year > 2019
+            #     and sum(
+            #         1
+            #         for _ in filter(
+            #             status_to_filter_regex.match,
+            #             [evolution.get_status().name for evolution in form_data.evolution],
+            #         )
+            #     )
+            #     > 0
+            # ):
+
+            # I replaced the above commented code with the following one. It's working.
+            # What follows matching_count verif in the historical code, refactored it
+            if creation_date.year > 2019:
+                status_names = [
+                    status.name
+                    for evolution in form_data.evolution
+                    if (status := evolution.get_status()) is not None
+                    and status.name is not None
+                ]
+
+                matching_count = sum(
+                    1 for _ in filter(status_to_filter_regex.match, status_names)
                 )
-                columns.append(last_update_date.strftime("%d/%m/%Y %H:%M:%S"))
-                columns.append(
-                    form_data.user.get_display_name() if form_data.user else "-"
-                )
-                for field in form_def.get_all_fields():  # les autres champs
-                    if not hasattr(field, "get_view_value"):  # sauf les titres, etc.
-                        continue
-                    if hasattr(field, "in_listing") and field.in_listing:
-                        field_value = form_data.get_field_view_value(field) or ""
-                    if hasattr(field, "include_in_listing") and (
-                        field.include_in_listing
-                        or field.varname == "nom_eleve"
-                        or field.varname == "prenom_eleve"
-                    ):
-                        field_value = form_data.get_field_view_value(field) or ""
-                        if (
-                            hasattr(field, "get_display_value")
-                            and field.get_display_value(
-                                form_data.get_field_view_value(field)
-                            )
-                            is not None
-                        ):
-                            field_value = field.get_display_value(
-                                form_data.get_field_view_value(field)
-                            )
-                        columns.append(field_value)
-                if path.exists(SAVE_FILE_PATH):
-                    FILE_OPEN_METHOD = "a"
-                with open(
-                    SAVE_FILE_PATH, FILE_OPEN_METHOD, encoding="utf-8"
-                ) as csvfile:
-                    csvwriter = csv.writer(
-                        csvfile,
-                        delimiter="|",
-                        quoting=csv.QUOTE_MINIMAL,
-                        lineterminator="\n",
+
+                if matching_count > 0:
+                    columns = []
+                    columns.append(
+                        form_data.get_display_id()
+                    )  # identifiant du formulaire
+                    columns.append(creation_date.strftime("%d/%m/%Y %H:%M:%S"))
+                    last_update_date = datetime.fromtimestamp(
+                        mktime(form_data.last_update_time)
                     )
-                    csvwriter.writerow(columns)
+                    columns.append(last_update_date.strftime("%d/%m/%Y %H:%M:%S"))
+                    columns.append(
+                        form_data.user.get_display_name() if form_data.user else "-"
+                    )
+                    for field in form_def.get_all_fields():  # les autres champs
+                        if not hasattr(
+                            field, "get_view_value"
+                        ):  # sauf les titres, etc.
+                            continue
+                        if hasattr(field, "in_listing") and field.in_listing:
+                            field_value = form_data.get_field_view_value(field) or ""
+                        if hasattr(field, "include_in_listing") and (
+                            field.include_in_listing
+                            or field.varname == "nom_eleve"
+                            or field.varname == "prenom_eleve"
+                        ):
+                            field_value = form_data.get_field_view_value(field) or ""
+                            if (
+                                hasattr(field, "get_display_value")
+                                and field.get_display_value(
+                                    form_data.get_field_view_value(field)
+                                )
+                                is not None
+                            ):
+                                field_value = field.get_display_value(
+                                    form_data.get_field_view_value(field)
+                                )
+                            columns.append(field_value)
+                    if path.exists(save_file_path):
+                        file_open_method = "a"
+                    with open(
+                        save_file_path, file_open_method, encoding="utf-8"
+                    ) as csvfile:
+                        csvwriter = csv.writer(
+                            csvfile,
+                            delimiter="|",
+                            quoting=csv.QUOTE_MINIMAL,
+                            lineterminator="\n",
+                        )
+                        csvwriter.writerow(columns)
+
+
+if __name__ == "__main__":
+    main()
