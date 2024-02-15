be90d79a793df8be86664853090bf06e2020f28f
Daniel Muyshond <daniel.muyshond@imio.be>
2024-02-15 11:10:39 +0100

    Replace wcsctl soon deprecated / unavailable, by  wcs-manage
    TELE-581

--- teleservices/hooks/waterlooteleservices/centre_recreatif_facture.py
+++ teleservices/hooks/waterlooteleservices/centre_recreatif_facture.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2
-# sudo -u wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=waterloo-formulaires.guichet-citoyen.be centre_recreatif_facture.py waterloo_facture
+# sudo -u  wcs wcs-manage runscript --vhost=$1-formulaires.$2
+# sudo -u wcs wcs-manage runscript --vhost=waterloo-formulaires.guichet-citoyen.be centre_recreatif_facture.py waterloo_facture
 
 import csv
 import re
