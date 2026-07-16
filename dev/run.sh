4ab6e6ba043eb46b735ea0f8c2f47ef809f69b51
Rémi Dubois <remi.dubois@gmail.com>
2026-07-16 09:49:01 +0200

    fix: spooler folder permissions
    we need to fix permissions since https://git.entrouvert.org/entrouvert/combo/commit/5ab0c3e114c091b154dd98d6a55aa0b13bef5bcf and https://git.entrouvert.org/entrouvert/passerelle/commit/6ee9bf73ee11b04a3606e7eba866c02e800fa505
    
    DEVOPS-397

--- dev/run.sh
+++ dev/run.sh
@@ -20,8 +20,8 @@ echo -n "$prefix updating some Entr'Ouvert services folders user:group via chown
 chown authentic-multitenant:authentic-multitenant /var/lib/authentic2-multitenant/tenants -R &&
 chown hobo:hobo /var/lib/hobo/tenants -R &&
 chown chrono:chrono /var/lib/chrono/tenants -R &&
-chown combo:combo /var/lib/combo/tenants -R &&
-chown passerelle:passerelle /var/lib/passerelle/tenants -R &&
+chown combo:combo /var/lib/combo -R &&
+chown passerelle:passerelle /var/lib/passerelle -R &&
 chown wcs:wcs /var/lib/wcs -R &&
 echo " done! ✅"
 
